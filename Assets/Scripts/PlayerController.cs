bff18fbfd4ed6fff7e6d7cecd20e1939d241ef84
Abdul Rafi <63101627+TakeMeGH@users.noreply.github.com>
2024-06-19 10:08:51 +0700

    feat: player animation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,6 +6,8 @@ public class PlayerController : MonoBehaviour
     Rigidbody _rigidbody;
     float _moveValue = 0;
     [SerializeField] float _maxSpeed;
+    [SerializeField] Animator _animator;
+
     void Start()
     {
         _rigidbody = GetComponent<Rigidbody>();
@@ -19,6 +21,23 @@ public class PlayerController : MonoBehaviour
     private void HandleMove()
     {
         _rigidbody.AddForce(new Vector3(_maxSpeed * _moveValue - GetPlayerHorizontalVelocity(), 0, 0), ForceMode.VelocityChange);
+        HandleMoveAnimation();
+    }
+
+    void HandleMoveAnimation()
+    {
+        if (_moveValue > 0)
+        {
+            _animator.Play("MoveRight");
+        }
+        else if (_moveValue < 0)
+        {
+            _animator.Play("MoveLeft");
+        }
+        else
+        {
+            _animator.Play("Idle");
+        }
     }
 
     public void OnThrowFood()
